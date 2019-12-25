c9dbb8db82ec99cb30a77d7ebb26a5181d6efb53
Maxim Bondarenko <bondarenko.maxim@gmail.com>
2019-12-26 00:56:30 +0200

    preprocessed files, strings, doc on csharp

--- 01-HelloWorld/hello.cpp
+++ 01-HelloWorld/hello.cpp
@@ -1,6 +1,25 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
 char const* greet()
 {
    return "hello, world";
+}
+
+void say(const std::string& msg)
+{
+    std::cout << msg << std::endl;
+}
+
+std::int32_t sum(const std::vector<std::int32_t>& ints)
+{
+    return std::accumulate(ints.begin(), ints.end(), 0);
+}
+
+std::string reverse(const std::string& s)
+{
+    return {s.rbegin(), s.rend()};
 }
 
 #include <boost/python.hpp>
@@ -9,4 +28,7 @@ BOOST_PYTHON_MODULE(hello)
 {
     using namespace boost::python;
     def("greet", greet);
+    def("say", say);
+    def("sum", sum);
+    def("reverse", reverse);
 }
